f1d49621a8d6a3e7bf2316ee18f527f7c150ae67
Lautaro Di Sanza <lautarod@gmail.com>
2025-05-09 20:12:22 -0300

    saveMemberOnChain

--- app/lib/semaphore.ts
+++ app/lib/semaphore.ts
@@ -6,6 +6,7 @@ import { verifyNoirProof } from "@semaphore-protocol/proof"
 import { SemaphoreSubgraph } from "@semaphore-protocol/data"
 import { SupportedNetwork } from "@semaphore-protocol/utils"
 import { LocalStorageKeys } from "./types"
+import { TransactionReceipt } from "ethers";
 
 const reCreateGroup = async (idGroup:string,network:SupportedNetwork) => {
     const semaphoreSubgraph = new SemaphoreSubgraph(network)
@@ -33,7 +34,31 @@ export async function addIdentityToCuratorsGroup() {
     group.addMember(identity.commitment);
     const proof: SemaphoreNoirProof = await generateNoirProof(identity, group, "add member to curator group", group.root)
     console.log("identity to group proof", proof)
+    console.log("saveMemberOnChain ...", proof)
+    const response_tx=await saveMemberOnChain(SEMAPHORE_CURATOR_GROUP_ID,identity.commitment)
+    console.log("saveMemberOnChain ... successfull. Status of tx", response_tx.status)
     return proof;
+}
+
+const saveMemberOnChain = async (idGroup:string,identityCommitment:bigint) : Promise<TransactionReceipt>  => {
+
+    const url = process.env.NEXT_PUBLIC_SEMAPHORE_RELAYER + "semaphore/addMember"
+
+    const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+    "groupId": Number(idGroup),
+    "identityCommitment": identityCommitment.toString()
+  }),
+  });
+
+  if (!response.ok) {
+    const errorMessage = await response.text();
+    console.error(`Call to semaphore/addMember API failed: ${errorMessage}`);
+    throw new Error("Call to semaphore/addMember API failed");
+  }
+  return response.json()
 }
 
 export function saveIdentity(identity: Identity) {
